f23b6c4eb2f32f34c09d0782e3bd045451058b73
Lee <leeuklee@gmail.com>
2021-02-26 18:26:18 +0100

    added ifttt messages

--- send_ifttt_message.py
+++ send_ifttt_message.py
@@ -2,13 +2,13 @@ import urequests as urequests
 
 import credentials
 
-url = 'http://maker.ifttt.com/trigger/{}/with/key/{}'
+ifttt_url = 'http://maker.ifttt.com/trigger/{}/with/key/{}'
 
 
 def send_message(sensor_readings):
     request_headers = {'Content-Type': 'application/json'}
 
-    request = urequests.post(url.format(credentials.ifttt_webhook_name, credentials.ifttt_api_key),
+    request = urequests.post(ifttt_url.format(credentials.ifttt_webhook_name, credentials.ifttt_api_key),
                              json=sensor_readings,
                              headers=request_headers)
     print(request.text)
