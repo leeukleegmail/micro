917251e5c951b5be03928c78da440e7f1f853d09
Lee <leeuklee@gmail.com>
2021-02-27 18:03:14 +0100

    more changes

--- config_template.py
+++ config_template.py
@@ -3,7 +3,10 @@
 # import credentials
 # credentials.ssid, credentials.password
 
+network_enabled = True
 ssid = "xy"
 password = "abcd"
+ifttt_enabled = True
 ifttt_api_key = "1234"
 ifttt_webhook_name = "9876"
+temperature_enabled = True
